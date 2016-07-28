2a3e61284a4d009dd36799d1eb7d71db33f3403c
ITHACA <info@ithacaweb.org>
2016-07-28 09:44:34 +0000

    adapted to geonode24

--- geonode_bck.sh
+++ geonode_bck.sh
@@ -5,24 +5,23 @@ echo "-----------------------------------------------------------------"
 echo "*****************************************************************"
 
 echo
-echo $(date) "--> starting bck for geonode dbs"
 
-sudo mount /dev/xvdg /mnt/auto_bck
-
-today_dir=/mnt/auto_bck/pg_dumps/$(date +"%Y%m%d")_dump
+#sudo mount /dev/xvdg /mnt/auto_bck
+bck_root=/mnt/data/auto-bck
+pg_bck_root=$bck_root/pg_dumps
+today_dir=$pg_bck_root/$(date +"%Y%m%d")_dump
 echo "creating backup folder -->" $today_dir
 mkdir $today_dir
 
 echo $(date) "--> dumping geonode db"
-pg_dump -Fc -U geonode geonode > $today_dir/geonode.dump
+pg_dump -Fc -U postgres geonode > $today_dir/geonode.dump
 echo $(date) "--> geonode db dumped"
 
-echo $(date) "--> dumping geonode_imports db"
-pg_dump -Fc -b -U geonode geonode_imports > $today_dir/geonode_imports.dump
-echo $(date) "--> geonode_imports db dumped"
+echo $(date) "--> dumping geonode_data db"
+pg_dump -Fc -b -U postgres geonode_data > $today_dir/geonode_data.dump
+echo $(date) "--> geonode_data db dumped"
 
 # removing old folders
-bck_dir=/mnt/auto_bck/pg_dumps
 
 # get current month
 cur_m=$(date +%m)
@@ -33,11 +32,11 @@ prev_m2=$(printf "%02d" $prev_m)
 # build re that matches current and previous month backups
 re=$(echo "201[6-9]("$cur_m"|"$prev_m2")")
 # loop on backups in days other than 01, 08, 15, 23
-for dir in $(ls $bck_dir | head -n -7 | grep -E $re | grep -Ev \
+for dir in $(ls $pg_bck_root | head -n -7 | grep -E $re | grep -Ev \
 '(01|08|15|23)_dump')
 do
  echo removing $dir
- rm -r $(echo $bck_dir"/"$dir)
+ rm -r $(echo $pg_bck_root"/"$dir)
 done
 
 echo "end of dumping procedure"
@@ -45,21 +44,23 @@ echo "end of dumping procedure"
 echo "-----------------------------------------------------------------"
 echo $(date) "--> starting bck for geoserver data folder"
 
-this_week_dir=/mnt/auto_bck/geoserver_bck/$(date +"%Yweek%W")
+data_dir_parent=/mnt/data
+gs_bck_root=$bck_root/geoserver_bck
+this_week_dir=$gs_bck_root/$(date +"%Yweek%W")
 echo "checking backup folder -->" $this_week_dir
 
 if [ -e $this_week_dir ]
 then
   echo folder exists
   echo $(date) "--> creating incremental archive"
-  sudo tar -cpzf $this_week_dir/incr_dump.tgz -C /mnt -g $this_week_dir/tarlog.snap --backup=numbered geoserver_data
+  sudo tar -cpzf $this_week_dir/incr_dump.tgz -C $data_dir_parent -g $this_week_dir/tarlog.snap --backup=numbered geoserver-data
   sudo cp $this_week_dir/tarlog_lev0.snap $this_week_dir/tarlog.snap
   echo $(date) "--> incremental archive created"
 else
   echo creating folder
   mkdir $this_week_dir
   echo $(date) "--> creating archive"
-  sudo tar -cpzf $this_week_dir/full_dump.tgz -C /mnt -g $this_week_dir/tarlog.snap geoserver_data
+  sudo tar -cpzf $this_week_dir/full_dump.tgz -C $data_dir_parent -g $this_week_dir/tarlog.snap geoserver-data
   sudo cp $this_week_dir/tarlog.snap $this_week_dir/tarlog_lev0.snap
   echo $(date) "--> archive created"
 fi
@@ -74,46 +75,16 @@ do
 done
 
 wre=$(echo "201[6-9]week("$ws")")
-for dir in $(ls $bck_dir | head -n -3 | grep -Ev $wre)
+for dir in $(ls $gs_bck_root | head -n -3 | grep -Ev $wre)
 do
  echo removing $dir
- sudo rm -r $(echo $bck_dir"/"$dir)
+ sudo rm -r $(echo $gs_bck_root"/"$dir)
 done
 
 echo "-----------------------------------------------------------------"
-echo $(date) "--> starting bck static folder"
-
-sthis_week_dir=/mnt/auto_bck/geonode_statics/$(date +"%Yweek%W")
-echo "checking backup folder -->" $sthis_week_dir
-
-if [ -e $sthis_week_dir ]
-then
-  echo folder exists
-else
-  echo creating folder
-  mkdir $sthis_week_dir
-fi
-
-echo $(date) "--> starting copy"
-cp -L -R --preserve=all -u --backup=numbered /var/www/geonode/ $sthis_week_dir
-echo $(date) "--> copy complete"
-
-bck_dir=/mnt/auto_bck/geonode_statics
-
-ws=$(echo 01)
-for w in $(seq -w 5 4 52)
-do
- ws=$(echo $ws"|"$w)
-done
-
-wre=$(echo "201[6-9]week("$ws")")
-for dir in $(ls $bck_dir | head -n -3 | grep -Ev $wre)
-do
- echo removing $dir
- rm -r $(echo $bck_dir"/"$dir)
-done
-
-
-sudo umount -d /dev/xvdg
+echo $(date) "--> starting geonode backup"
+python /home/ubuntu/geonode_bck/backup-restore/backup.py
+mv /home/ubuntu/backup/* /mnt/data/auto-bck/static
+echo $(date) "--> geonode backup complete"
 
 echo "end of procedure"
